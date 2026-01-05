4898d7a3264b99e8aa8d52fe0fcae1b29120267b
徐启深 <xuqishen@nudt.edu.cn>
2026-01-05 10:53:27 +0800

    'xqs202601051053'

--- core/config.py
+++ core/config.py
@@ -1,16 +1,18 @@
 # 配置文件，包含API地址、Key、模型名等
 
-ATTACKER_API_URL = "https://yunwu.ai/v1/chat/completions"
-ATTACKER_API_KEY = "sk-mBv9A5UrRCQ7OzJDPLKZkjIRzoywwVcu4wvStR4P6E7K9Kw9"
-ATTACKER_MODEL_NAME = "gemini-3-pro-preview"
+ATTACKER_API_URL = "YOUR_URL"
+ATTACKER_API_KEY = "YOUR_API_KEY"
+ATTACKER_MODEL_NAME = "YOUR_MODEL_NAME"
 
-ANALYST_API_URL = "https://yunwu.ai/v1/chat/completions"
-ANALYST_API_KEY = "sk-mBv9A5UrRCQ7OzJDPLKZkjIRzoywwVcu4wvStR4P6E7K9Kw9"
-ANALYST_MODEL_NAME = "gemini-3-pro-preview"
+ANALYST_API_URL = "YOUR_URL"
+ANALYST_API_KEY = "YOUR_API_KEY"
+ANALYST_MODEL_NAME = "YOUR_MODEL_NAME"
 
-TARGET_API_URL = "https://yunwu.ai/v1/chat/completions"
-TARGET_API_KEY = "sk-mBv9A5UrRCQ7OzJDPLKZkjIRzoywwVcu4wvStR4P6E7K9Kw9"
-TARGET_MODEL_NAME = "gpt-4-turbo"
+TARGET_API_URL = "YOUR_URL"
+TARGET_API_KEY = "YOUR_API_KEY"
+TARGET_MODEL_NAME = "YOUR_MODEL_NAME"
 
+
+# We use llama-guard3 as a local safety filter, you can replace it with your own safety filter service.
 LLAMA_GUARD3_URL = "http://localhost:11434/api/generate"
 LLAMA_GUARD3_MODEL = "llama-guard3:latest"
